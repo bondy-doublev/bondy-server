52c9aeb484c86a64cca2a49c62febe0ede2eddf1
vixtruong <vipro20161@gmail.com>
2025-09-28 16:37:31 +0700

    feat: mail-service logic

--- services/auth-service/src/main/resources/db/migration/V2__create_intern_apikey.sql
+++ services/auth-service/src/main/resources/db/migration/V2__create_intern_apikey.sql
@@ -1,7 +1,7 @@
 INSERT INTO api_keys (name, key_hash, prefix, expires_at, active, created_at)
 VALUES (
            'internal-key',
-           '36f053f2f4f4cc612155ad84999c93c308ed73592b9d53701c9d586cf6cb1b98',
+           '213886978f99ddad7fc449d9031fed6013eda1231aa9a016471879365aa91dbb',
            'internal',
            NULL,
            TRUE,
